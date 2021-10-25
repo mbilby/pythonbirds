0ff92239da8fea177b5cab8336e70583a3515eec
Marcelo Barbosa <mbb.hef@uea.edu.br>
2021-10-25 11:51:53 -0400

    Created Attribute of Instance

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -2,10 +2,17 @@ from typing import Mapping
 
 
 class Pessoa:
+    def __init__(self, nome=None, idade=35):
+        self.nome = nome
+        self.idade = idade
+
     def cumprimentar(self):
         return f'Olá, {id(self)}'
 
 
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('Fernanda')
     print(p.cumprimentar())
+    p.nome = 'Marcelo'
+    print(p.nome)
+    print(p.idade)
