a1502b0c0e2f02c8bdee724f1ecfc6b0c88d7e9e
Marcelo Barbosa <mbb.hef@uea.edu.br>
2021-10-25 12:08:57 -0400

    Created Attribute filho Complex

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -2,17 +2,23 @@ from typing import Mapping
 
 
 class Pessoa:
-    def __init__(self, nome=None, idade=35):
+    def __init__(self, *filhos, nome=None, idade=35):
         self.nome = nome
         self.idade = idade
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f'Olá, {id(self)}'
 
 
 if __name__ == '__main__':
-    p = Pessoa('Fernanda')
-    print(p.cumprimentar())
-    p.nome = 'Marcelo'
-    print(p.nome)
-    print(p.idade)
+    marcelo = Pessoa(nome='Marcelo')
+    fernanda = Pessoa(marcelo, nome='Fernanda')
+    print(Pessoa.cumprimentar(fernanda))
+#    p.nome = 'Marcelo'
+    print(id(fernanda))
+    print(fernanda.cumprimentar())
+    print(fernanda.nome)
+    print(fernanda.idade)
+    for filho in fernanda.filhos:
+        print(filho.nome)
